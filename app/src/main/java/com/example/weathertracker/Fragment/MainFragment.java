53606d61701e1b99510b6cc384fcf2aa5a06b9b2
Clark <49876290+CLARKCLARKCLARK@users.noreply.github.com>
2021-03-17 19:11:57 +0800

    linechart2/17

--- app/src/main/java/com/example/weathertracker/Fragment/MainFragment.java
+++ app/src/main/java/com/example/weathertracker/Fragment/MainFragment.java
@@ -1,5 +1,6 @@
 package com.example.weathertracker.Fragment;
 
+import android.graphics.Color;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
@@ -10,6 +11,15 @@ import android.widget.Toast;
 import androidx.fragment.app.Fragment;
 
 import com.example.weathertracker.R;
+import com.github.mikephil.charting.charts.LineChart;
+import com.github.mikephil.charting.components.Description;
+import com.github.mikephil.charting.data.Entry;
+import com.github.mikephil.charting.data.LineData;
+import com.github.mikephil.charting.data.LineDataSet;
+import com.github.mikephil.charting.interfaces.datasets.ILineDataSet;
+
+import java.util.ArrayList;
+import java.util.List;
 
 /**
  * A simple {@link Fragment} subclass.
@@ -35,7 +45,64 @@ public class MainFragment extends Fragment {
 
             }
         });
+
+
+        LineChart lineChart = root.findViewById(R.id.lineChart);
+
+        LineDataSet lineDataSet = new LineDataSet(lineChartDataSet(),"data set");
+        ArrayList<ILineDataSet> iLineDataSets = new ArrayList<>();
+        iLineDataSets.add(lineDataSet);
+
+        LineData lineData = new LineData(iLineDataSets);
+        lineChart.setData(lineData);
+        lineChart.invalidate();
+
+
+        //if you want set background color use below method
+        //lineChart.setBackgroundColor(Color.RED);
+
+        // set text if data are are not available
+        lineChart.setNoDataText("Data not Available");
+
+        //you can modify your line chart graph according to your requirement there are lots of method available in this library
+
+        //now customize line chart
+        Description description = lineChart.getDescription();
+        description.setText("美元(USD)");//顯示文字名稱
+        description.setTextSize(14);//字體大小
+        description.setTextColor(Color.BLUE);//字體顏色
+        description.setPosition(900, 80);
+
+
+        //設定沒資料時顯示的內容
+        lineChart.setNoDataText("暫時沒有數據");
+        lineChart.setNoDataTextColor(Color.BLUE);
+        lineDataSet.setColor(Color.BLUE);
+        lineDataSet.setCircleColor(Color.RED);
+        lineDataSet.setDrawCircles(true);
+        lineDataSet.setDrawCircleHole(true);
+        lineDataSet.setLineWidth(2);
+        lineDataSet.setCircleRadius(5);
+        lineDataSet.setCircleHoleRadius(2);
+        lineDataSet.setValueTextSize(10);
+        lineDataSet.setValueTextColor(Color.BLACK);
+
         return root;
+    }
+
+    private List<Entry> lineChartDataSet() {
+        ArrayList<Entry> dataSet = new ArrayList<Entry>();
+
+        dataSet.add(new Entry(0,40));
+        dataSet.add(new Entry(1,10));
+        dataSet.add(new Entry(2,15));
+        dataSet.add(new Entry(3,12));
+        dataSet.add(new Entry(4,20));
+        dataSet.add(new Entry(5,50));
+        dataSet.add(new Entry(6,23));
+        dataSet.add(new Entry(7,34));
+        dataSet.add(new Entry(8,12));
+        return  dataSet;
     }
 
 }
