8aecf8131464491425d4bf327738770b34bac944
SnaKey0u0 <48859497+SnaKey0u0@users.noreply.github.com>
2021-09-28 18:58:34 +0800

    民生示警、修正weather icon bug

--- app/src/main/java/com/example/weathertracker/account/ForgetPasswordActivity.java
+++ app/src/main/java/com/example/weathertracker/account/ForgetPasswordActivity.java
@@ -32,36 +32,36 @@ public class ForgetPasswordActivity extends AppCompatActivity {
         sendMail.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                Toast.makeText(ForgetPasswordActivity.this, "此功能將日後實裝", Toast.LENGTH_SHORT).show();
-//                if (android.util.Patterns.EMAIL_ADDRESS.matcher(email.getText().toString()).matches()) {
-//                    RetrofitService retrofitService = RetrofitManager.getInstance().getService();
-//                    Call<Ack> call = retrofitService.sendResetMail(email.getText().toString());
-//                    call.enqueue(new Callback<Ack>() {
-//                        @Override
-//                        public void onResponse(Call<Ack> call, Response<Ack> response) {
-//                            if (!response.isSuccessful()) {
-//                                Toast.makeText(ForgetPasswordActivity.this, "伺服器錯誤，請稍後再試", Toast.LENGTH_SHORT).show();
-//                            } else {
-//                                Ack ack = response.body();
-//                                if (ack.getCode() == 200) {
-//                                    Toast.makeText(ForgetPasswordActivity.this, ack.getMsg(), Toast.LENGTH_SHORT).show();//去信箱收信
-//                                    Intent intent = new Intent(ForgetPasswordActivity.this, LoginActivity.class);
-//                                    startActivity(intent);
-//                                    finish();
-//                                } else {
-//                                    Toast.makeText(ForgetPasswordActivity.this, "錯誤訊息: " + ack.getMsg(), Toast.LENGTH_SHORT).show();
-//                                }
-//                            }
-//                        }
-//
-//                        @Override
-//                        public void onFailure(Call<Ack> call, Throwable t) {
-//                            Toast.makeText(ForgetPasswordActivity.this, "連線錯誤，請稍後再試", Toast.LENGTH_SHORT).show();
-//                        }
-//                    });
-//                } else {
-//                    Toast.makeText(ForgetPasswordActivity.this, "信箱格式不正確", Toast.LENGTH_SHORT).show();
-//                }
+//                Toast.makeText(ForgetPasswordActivity.this, "此功能將日後實裝", Toast.LENGTH_SHORT).show();
+                if (android.util.Patterns.EMAIL_ADDRESS.matcher(email.getText().toString()).matches()) {
+                    RetrofitService retrofitService = RetrofitManager.getInstance().getService();
+                    Call<Ack> call = retrofitService.sendResetMail(email.getText().toString());
+                    call.enqueue(new Callback<Ack>() {
+                        @Override
+                        public void onResponse(Call<Ack> call, Response<Ack> response) {
+                            if (!response.isSuccessful()) {
+                                Toast.makeText(ForgetPasswordActivity.this, "伺服器錯誤，請稍後再試", Toast.LENGTH_SHORT).show();
+                            } else {
+                                Ack ack = response.body();
+                                if (ack.getCode() == 200) {
+                                    Toast.makeText(ForgetPasswordActivity.this, ack.getMsg(), Toast.LENGTH_SHORT).show();//去信箱收信
+                                    Intent intent = new Intent(ForgetPasswordActivity.this, LoginActivity.class);
+                                    startActivity(intent);
+                                    finish();
+                                } else {
+                                    Toast.makeText(ForgetPasswordActivity.this, "錯誤訊息: " + ack.getMsg(), Toast.LENGTH_SHORT).show();
+                                }
+                            }
+                        }
+
+                        @Override
+                        public void onFailure(Call<Ack> call, Throwable t) {
+                            Toast.makeText(ForgetPasswordActivity.this, "連線錯誤，請稍後再試", Toast.LENGTH_SHORT).show();
+                        }
+                    });
+                } else {
+                    Toast.makeText(ForgetPasswordActivity.this, "信箱格式不正確", Toast.LENGTH_SHORT).show();
+                }
             }
         });
     }
